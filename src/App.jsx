dfc4ef3c3592e24ce8420918d7fe0a82ecbccb49
Abhishek <abhi.ghosh27feb@gmail.com>
2021-09-25 19:48:32 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { Route, Switch } from "react-router";
+import { Router } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
+import history from "./history";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+
+  render() {
+    return (
+      <Router history={history}>
+        <NavBar isLoggedIn={this.state.isLoggedIn} />
+        <div className="container-fluid">
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <Login
+                  {...props}
+                  updateIsLoggedInStatus={this.updateIsLoggedInStatus}
+                />
+              )}
+            />
+            <Route path="/dashboard" exact component={Dashboard} />
+            <Route path="/customers" exact component={CustomersList} />
+            <Route path="/cart" exact component={ShoppingCart} />
+            <Route path="*" component={NoMatchPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
+  //This method can be called by child components to update isLoggedIn property of the state
+  updateIsLoggedInStatus = (status) => {
+    this.setState({ isLoggedIn: status });
+  };
+}
